0927fa5a14982d36b5059b20a3d0653acbc15178
YavorYanchev <yavor_yanchev_bs@abv.bg>
2018-06-22 10:27:18 +0300

    Updated homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,14 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { User } from '../_models';
 
-@Component({templateUrl: 'home.component.html'})
+@Component({templateUrl: 'home.component.html',styleUrls: ['./home.component.css']})
 export class HomeComponent implements OnInit {
     currentUser: User;
-
+    
     constructor() {
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        //TODO don't do it here
+
     }
 
     ngOnInit() {}
